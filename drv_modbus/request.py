69780aaebb0a3b44eb3e92231687c142e502bbd4
kevin00156 <qazsskevin@gmail.com>
2024-09-11 13:57:08 +0800

    update commit

--- drv_modbus/request.py
+++ drv_modbus/request.py
@@ -2,12 +2,23 @@ from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.constants import Endian
 
 def Get_TCP_Pose(robot_client):
-    request = robot_client.read_holding_registers(0x00F0, 12, 2)  
+    """
+    從機械手臂讀取 TCP 位姿 (X, Y, Z, Rx, Ry, Rz)
+    
+    參數:
+        robot_client: Modbus TCP 客戶端，用於與機械手臂通信
+        
+    返回:
+        x, y, z: 機械手臂末端執行器的空間座標
+        rx, ry, rz: 機械手臂末端的旋轉角度 (Rx, Ry, Rz)
+    """
+    request = robot_client.read_holding_registers(0x00F0, 12, 2)  # 讀取 12 個保持寄存器
 
     if request.isError():
         print("request error!")
         return
 
+    # 從寄存器中解碼 X, Y, Z, Rx, Ry, Rz 的值
     decoder_x = BinaryPayloadDecoder.fromRegisters(request.registers[:2], Endian.BIG, wordorder=Endian.LITTLE)
     decoder_y = BinaryPayloadDecoder.fromRegisters(request.registers[2:4], Endian.BIG, wordorder=Endian.LITTLE)
     decoder_z = BinaryPayloadDecoder.fromRegisters(request.registers[4:6], Endian.BIG, wordorder=Endian.LITTLE)
@@ -15,6 +26,7 @@ def Get_TCP_Pose(robot_client):
     decoder_ry = BinaryPayloadDecoder.fromRegisters(request.registers[8:10], Endian.BIG, wordorder=Endian.LITTLE)
     decoder_rz = BinaryPayloadDecoder.fromRegisters(request.registers[10:], Endian.BIG, wordorder=Endian.LITTLE)
 
+    # 解析每個座標，並將其縮放到米
     x  = decoder_x.decode_32bit_int() * 0.001
     y  = decoder_y.decode_32bit_int() * 0.001
     z  = decoder_z.decode_32bit_int() * 0.001
@@ -25,7 +37,16 @@ def Get_TCP_Pose(robot_client):
     return x , y , z , rx, ry, rz
 
 def Get_Pose_Flag(robot_client):
-    request = robot_client.read_holding_registers(0x031F, 1, 2)  
+    """
+    獲取當前機械手臂的位姿狀態標誌
+    
+    參數:
+        robot_client: Modbus TCP 客戶端
+    
+    返回:
+        pose_flag: 表示位姿的標誌
+    """
+    request = robot_client.read_holding_registers(0x031F, 1, 2)  # 讀取指定寄存器的位姿狀態標誌
     if request.isError():
         print("request error!")
         return
--- drv_modbus/send.py
+++ drv_modbus/send.py
@@ -5,26 +5,43 @@ from drv_modbus import request
 MovP = 0
 MovL = 1
 
-def Go_Position(c, x, y, z, rx, ry, rz, speed = 20, mov = 0, block = True):
+def Go_Position(c, x, y, z, rx, ry, rz, speed=20, mov=0, block=True):
+    """
+    讓機械手臂移動到指定的位置 (X, Y, Z, Rx, Ry, Rz)
+
+    參數:
+        c: Modbus TCP 客戶端
+        x, y, z: 移動到的目標座標 (米)
+        rx, ry, rz: 旋轉角度 (弧度)
+        speed: 移動速度 (預設為 20)
+        mov: 移動方式 (MovP 或 MovL)
+        block: 是否阻塞直到動作完成 (預設為 True)
+    """
+    # 將座標和旋轉角度轉換為寄存器數據格式
     builder = BinaryPayloadBuilder(byteorder=Endian.BIG, wordorder=Endian.LITTLE)
-    builder.add_32bit_int(int(x  * 1000))
-    builder.add_32bit_int(int(y  * 1000))
-    builder.add_32bit_int(int(z  * 1000))
+    builder.add_32bit_int(int(x * 1000))
+    builder.add_32bit_int(int(y * 1000))
+    builder.add_32bit_int(int(z * 1000))
     builder.add_32bit_int(int(rx * 1000))
     builder.add_32bit_int(int(ry * 1000))
     builder.add_32bit_int(int(rz * 1000))
     payload = builder.to_registers()
+
+    # 設置移動速度和目標位姿
     c.write_register(0x0324, speed, 2)
     c.write_registers(0x0330, payload, 2)
 
+    # 執行 MovP 或 MovL 動作
     if mov == MovP:
         print("MovP")
-        c.write_register(0x0300, 301, 2)   
+        c.write_register(0x0300, 301, 2)
     if mov == MovL:
         print("MovL")
-        c.write_register(0x0300, 302, 2)   
+        c.write_register(0x0300, 302, 2)
 
     print("Start moving...")
+
+    # 若 block 為 True，等待移動完成
     if block == False:
         return
 
@@ -34,41 +51,63 @@ def Go_Position(c, x, y, z, rx, ry, rz, speed = 20, mov = 0, block = True):
     print("Move done!")
 
 def Suction_ON(c):
-    c.write_register(0x02FE, 1, 2)   
+    """
+    打開吸盤
+
+    參數:
+        c: Modbus TCP 客戶端
+    """
+    c.write_register(0x02FE, 1, 2)
 
 def Suction_OFF(c):
-    c.write_register(0x02FE, 0, 2)   
+    """
+    關閉吸盤
+
+    參數:
+        c: Modbus TCP 客戶端
+    """
+    c.write_register(0x02FE, 0, 2)
 
 def Jog_Position(c, x, y, z, rx, ry, rz):
+    """
+    通過手動模式 (Jog) 控制機械手臂的移動
+
+    參數:
+        c: Modbus TCP 客戶端
+        x, y, z: 控制手臂在空間中的移動方向 (-1 或 1)
+        rx, ry, rz: 控制手臂的旋轉方向
+    """
+    # 根據方向參數控制各個方向的 Jog 移動
     if x > 0:
         c.write_registers(0x0300, 601, 2)
     if x < 0:
         c.write_registers(0x0300, 602, 2)
-
     if y > 0:
         c.write_registers(0x0300, 603, 2)
     if y < 0:
         c.write_registers(0x0300, 604, 2)
-
     if z > 0:
         c.write_registers(0x0300, 605, 2)
     if z < 0:
         c.write_registers(0x0300, 606, 2)
-
     if rx > 0:
         c.write_registers(0x0300, 607, 2)
     if rx < 0:
         c.write_registers(0x0300, 608, 2)
-
     if ry > 0:
         c.write_registers(0x0300, 609, 2)
     if ry < 0:
         c.write_registers(0x0300, 610, 2)
-
     if rz > 0:
         c.write_registers(0x0300, 611, 2)
     if rz < 0:
         c.write_registers(0x0300, 612, 2)
 
 def Jog_Stop(c):
+    """
+    停止機械手臂的 Jog 模式運動
+
+    參數:
+        c: Modbus TCP 客戶端
+    """
     c.write_registers(0x0300, 0, 2)
