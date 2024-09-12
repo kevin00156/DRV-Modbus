4dc79f9de919dfea8bb629ba7c921d9b0de79ffa
kevin00156 <qazsskevin@gmail.com>
2024-09-12 16:42:33 +0800

    優化drv_modbus_example.py的輸出方式

--- drv_modbus_example.py
+++ drv_modbus_example.py
@@ -6,9 +6,14 @@ import time
 
 if __name__ == "__main__":
     # 建立modbus客戶端並連接到指定IP的設備
-    c = ModbusTcpClient(host="192.168.1.1", port=502)
-    c.connect()
-    print ("Connected to arm {c.address}:{port}")
+    host = "192.168.1.1"
+    port = 502
+    c = ModbusTcpClient(host=host, port=port)
+    if not c.connect():
+        print(f"cannot connect to {host}:{port}")
+        exit(1)
+    else:
+        print(f"Connected to {host}:{port}")
 
     # 定義機械手臂的home點的姿態 (x, y, z, rx, ry, rz)
     home = [408.285, 0.0, 680.0120000000001, 178.969, -0.241, -103.145]
@@ -23,8 +28,10 @@ if __name__ == "__main__":
 
         print(f"x: {x}, y: {y}, z: {z}, rx: {rx}, ry: {ry}, rz: {rz}")
 
+        if x==home[0] and y==home[1] and z==home[2] and rx==home[3] and ry==home[4] and rz==home[5]:
+            print ("position arrived!")
+            break
         # 每隔0.2秒打印一次手臂位置
         time.sleep(0.2)
-
     # 程式結束，關閉 modbus 連接
     c.close()
