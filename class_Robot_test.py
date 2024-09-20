85d3ec446eab424480736722c7c5b2ca0247e890
kevin00156 <qazsskevin@gmail.com>
2024-09-20 17:37:23 +0800

    這個版本仍未測試 新增了robot這個class，以更方便的控制機器人 並新建了一個class_Robot_test.py檔案，用以測試class是否正常運作

---
+++ class_Robot_test.py
@@ -0,0 +1,19 @@
+
+from robot.Class_Robot import Robot
+from pymodbus.client import ModbusTcpClient
+from robot.Class_Robot import eRobotCommand
+
+if __name__   == "__main__":
+    
+    host = "192.168.1.1"
+    modbusTCPClient = ModbusTcpClient(host= host)
+    
+    
+    robotDRV = Robot(modbusTCPClient)
+    
+    robotDRV.Send_Motion_Command(eRobotCommand.Robot_All_Joints_Homing_To_Origin)#測試回home功能
+
+    while True:
+        print(robotDRV.Get_TCP_Pose)
+        if robotDRV.isRobotReachTargetPosition :
+            break
